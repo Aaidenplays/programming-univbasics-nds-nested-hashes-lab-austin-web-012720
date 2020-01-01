41c305765d9e8f2540ed628130469b54d050740a
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-01 22:59:40 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,11 +18,11 @@ def literal_aoh
   # held in the constants
   names_list = assembled_aoh
   names_list = [
-            Don_G = {name: "Don Gately", occopation: "Live-in Staff"},
-            JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-            PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-            KATE_G = { name:  "Kate Gompert", occupation: "None" },
-            BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred"} 
+            {name: "Don Gately", occopation: "Live-in Staff"},
+            { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+            { name:  "Pat Monteseian", occupation: "Staff" },
+            { name:  "Kate Gompert", occupation: "None" },
+            { name:  "Bruce Green", occupation: "Fan of Mildred"} 
                 ]
   names_list.sort!
   binding.pry
