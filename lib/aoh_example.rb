050072cac6234c782c79f1ef16818d20a4d12d3d
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-01 22:59:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,9 +19,10 @@ def literal_aoh
   names_list = assembled_aoh
   names_list = [
             Don_G = {name: "Don Gately", occopation: "Live-in Staff"},
-            JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-            PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-            
+            JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+            PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+            KATE_G = { name:  "Kate Gompert", occupation: "None" },
+            BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred"} 
                 ]
   names_list.sort!
   binding.pry
